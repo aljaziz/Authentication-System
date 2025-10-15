950d99a8e88ce585d46d5c2a440fdd258173a089
aljaziz <turjatheharbingerofdeath@gmail.com>
2025-10-15 19:05:27 +0600

    login completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -48,7 +48,39 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-    res.send("Login route");
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res
+                .status(400)
+                .json({ success: false, message: "Invalid credentials" });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res
+                .status(400)
+                .json({ success: false, message: "Invalid credentials" });
+        }
+
+        generateToken(res, user._id);
+
+        user.lasLogin = new Date();
+        await user.save();
+
+        res.status(201).json({
+            success: true,
+            message: "Logged in successfully",
+            user: { ...user._doc, password: undefined },
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            success: false,
+            message: error.message,
+        });
+    }
 };
 
 export const logout = async (req, res) => {
